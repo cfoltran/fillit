cc56b6f102024413720691ef22dab6aaad1ca11e
Clément FOLTRAN <clementfoltran@MacBook-Pro-de-Clement.local>
2018-11-28 23:39:48 +0100

    read doesn't not segfault

--- utils.c
+++ utils.c
@@ -1,15 +1,6 @@
 #include "fillit.h"
 #include <stdio.h>
 
-// void		ft_tetriadd(t_tetri **tetri, t_tetri *new)
-// {
-// 	if (new && tetri)
-// 	{
-// 		new->next = *tetri;
-// 		*tetri = new;
-// 	}
-// }
-
 t_tetri			*tetri_add(t_tetri *lst, char *line, char c)
 {
 	t_tetri		*tmp;
@@ -32,27 +23,11 @@ t_tetri			*tetri_add(t_tetri *lst, char *line, char c)
 	return (lst);
 }
 
-// t_tetri		*ft_tetrinew(char **tetri, char id, t_coord point)
-// {
-// 	t_tetri		*result;
-
-// 	if (!tetri || !id)
-// 		return (NULL);
-// 		if (!(result = (t_tetri*)malloc(sizeof(t_tetri))))
-// 		return (NULL);
-
-// 	result->tetri = tetri;
-// 	result->id = id;
-// 	result->point = point;
-// 	result->next = NULL;
-// 	return (result);
-// }
-
 t_tetri		*read_piece(int fd)
 {
 	t_tetri	*lst;
 	char	*line;
-	char	**tmp;
+	char	*tmp;
 	char	id;
 	int		cpt;
 
@@ -61,11 +36,11 @@ t_tetri		*read_piece(int fd)
 	lst = NULL;
 	while (get_next_line(fd, &line) == 1)
 	{
-		if (!(tmp = ()))
-		tmp = ft_strjoinfree(tmp, line, ft_strlen(line), 1);
+		tmp = ft_strnew(ft_strlen(line));
+		tmp = ft_strcpy(tmp, line);
 		if (++cpt % 5 == 0)
 		{
-		 	lst = tetri_add(lst, *tmp, id++);
+		 	lst = tetri_add(lst, tmp, id++);
 			ft_memdel((void *)&tmp);
 			ft_memdel((void *)&line);
 		}
