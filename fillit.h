3cca3631c0ed7b2939b4ae0b4e30a5bbd08088fb
owalid <othmane.el.ayadi@gmail.com>
2018-11-28 14:23:44 +0100

    ajout des utils pour les liste

--- fillit.h
+++ fillit.h
@@ -7,11 +7,19 @@
 # include <fcntl.h>
 # define BUFF_SIZE 21
 
-typedef struct 		s_list
+typedef struct 			s_coord
+{
+	int					x[4];
+	int					y[5];
+	struct s_coord		*next;
+}						t_coord;
+
+typedef struct 		s_tetri
 {
 	char			**tetri;
 	char			id;
+	t_coord			point;
 	struct s_list 	*next;
-}					t_list;
+}					t_tetri;
 
 #endif
--- utils.c
+++ utils.c
@@ -1,9 +1,28 @@
-t_list		*ft_lstnew(char	**tetri, char id)
+t_coord		*ft_coordnew(int x[4], int y[5])
+{
+	t_coord 	*result;
+
+	if (!(result = (t_coord)malloc(sizeof(t_coord))))
+	{
+		result->x = x;
+		result->y = y;
+	}
+	result->next = NULL;
+	return (result);
+}
+
+
+t_tetri		*ft_lstnew(char	**tetri, char id, t_coord point)
 {
 	t_list		*result;
 
+	if (!tetri || !id || !point)
+		return (NULL);
 	if (!(result = (t_list*)malloc(sizeof(t_list))))
 	{
-
+		result->tetri = tetri;
+		result->id = id;
 	}
+	result->next = NULL;
+	return (result);
 }
