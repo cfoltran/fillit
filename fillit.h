80d2f64caab01feebf9fa97d8fd8b00685c5637b
owalid <othmane.el.ayadi@gmail.com>
2018-11-29 13:26:41 +0100

    push avec add coord

--- fillit.h
+++ fillit.h
@@ -14,20 +14,22 @@ typedef struct 		s_coord
 {
 	int				x[4];
 	int				y[5];
+	struct s_coord	*next;
 }					t_coord;
 
 typedef struct		s_tetri
 {
 	char			**tetri;
 	char			id;
-	t_coord			point;
+	t_coord			*point;
 	struct s_tetri 	*next;
 }					t_tetri;
 
 //void				ft_tetriadd(t_tetri **tetri, t_tetri *new);
 //t_tetri				*ft_tettrinew(char **tetri, char id, t_coord point);
 t_tetri		        *read_piece(int fd);
-t_tetri				*tetri_add(t_tetri *lst, char *line, char c);
+t_tetri				*tetri_add(t_tetri *lst, char *line, char c, t_coord *point);
+void				coord_add(t_coord **cord, char *line, int cpt);
 int         		get_next_line(const int fd, char **line);
 
 #endif
